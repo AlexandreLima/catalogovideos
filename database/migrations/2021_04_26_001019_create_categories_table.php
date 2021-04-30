6f906325462bfd4c9df2d0e4f8cc900453eede2e
AlexandreLima <xandebre@gmail.com>
2021-04-29 23:21:19 -0300

    feat:

--- database/migrations/2021_04_26_001019_create_categories_table.php
+++ database/migrations/2021_04_26_001019_create_categories_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Ramsey\Uuid\Uuid;
 
 class CreateCategoriesTable extends Migration
 {
@@ -14,10 +15,11 @@ class CreateCategoriesTable extends Migration
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->uuid('id')->primary();;
             $table->string('name');
             $table->text('description')->nullable(true);
             $table->boolean('is_active')->default(true);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
